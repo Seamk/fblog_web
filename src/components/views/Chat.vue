bee105a12265e587b817436abaae999b17eccd95
fulei <fulei@yunjiacloud.com>
2019-12-03 18:38:04 +0800

    chat

---
+++ src/components/views/Chat.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <button @click="send">发消息</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return {
+        path:"ws://localhost:8080/",
+        socket:""
+      }
+    },
+    mounted() {
+      this.init()
+    },
+    methods:{
+      init:function () {
+        if(typeof(Websocket)==undefined ){
+
+        }
+      }
+    }
+  }
+</script>
--- src/components/views/Menu.vue
+++ src/components/views/Menu.vue
@@ -1,15 +1,25 @@
 <template>
   <div class="menu">
       {{msg}}
+      <confirm text="注册"  @message="getMessage"></confirm>
   </div>
 </template>
 
 <script>
+  import Confirm from '../sub/Confirm'
   export default{
     name:"Menu",
+    Components:{
+      Confirm
+    },
     data(){
       return{
         msg:"lalalal"
+      }
+    },
+    methods:{
+      getMessage(val){
+        alert(val);
       }
     }
   }
