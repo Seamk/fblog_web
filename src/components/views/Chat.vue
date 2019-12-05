1ff091550d8d67b0dce35379e2ec7e172a8dcd26
fulei <fulei@yunjiacloud.com>
2019-12-05 11:23:13 +0800

    chat

--- src/components/views/Chat.vue
+++ src/components/views/Chat.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
+    {{msg}}
     <button @click="send">发消息</button>
   </div>
 </template>
 
 <script>
   export default {
+    name: "Chat",
     data(){
       return {
-        path:"ws://localhost:8080/",
-        socket:""
+        path:"ws://localhost:8080/websocket/1",
+        socket:"",
+        msg:""
       }
     },
     mounted() {
@@ -17,10 +20,34 @@
     },
     methods:{
       init:function () {
-        if(typeof(Websocket)==undefined ){
-
+        if(typeof(WebSocket)==undefined ){
+            alert("您的浏览器不支持socket");
+        }else{
+          this.socket = new WebSocket(this.path);
+          this.socket.onopen = this.open;
+          this.socket.onerror = this.error;
+          this.socket.onmessage = this.getMessage;
         }
+      },
+      open:function(){
+        console.log("socket连接成功");
+      },
+      error:function(){
+        console.log("连接错误");
+      },
+      getMessage:function(msg){
+        this.msg+=msg.data;
+        console.log(msg.data);
+      },
+      send:function(){
+        this.socket.send(params);
+      },
+      close:function(){
+        console.log("socket已经关闭");
       }
+    },
+    destroyed() {
+      this.socket.onclose = this.close;
     }
   }
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Menu from '@/components/views/Menu'
+import Chat from '@/components/views/Chat'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path:'/menu',
       name:'menu',
       component: Menu
+    },
+    {
+      path:'/chat',
+      name:'chat',
+      component:Chat
     }
   ]
 })
